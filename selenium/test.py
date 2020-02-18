db90a73aaf6c8efb1886d4d0d430964ca8865ec2
Minh <hungquocminh1989@gmail.com>
2020-02-18 18:38:32 +0700

    add

---
+++ selenium/test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+#-----------------------------------------------------------------------------
+# Name:        
+#
+# Purpose:
+#
+# Version:     1.1
+#
+# Author:
+#
+# Created:     06/02/2020
+# Updated:     06/02/2020
+#
+# Copyright:   -
+#
+#-----------------------------------------------------------------------------
+"""
+
+#Import lib
+from selenium import webdriver
+
+browser = webdriver.Firefox(executable_path='X:/01-WIN10-TMP/Desktop/python_files/selenium/webdriver/firefox.exe')
+browser.get('http://seleniumhq.org/')
+
+exit()
