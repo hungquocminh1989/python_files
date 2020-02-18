122e9205da9588326b829c4d827fec15187b0ae1
Minh-Home <hungquocminh1989@gmail.com>
2020-02-18 23:56:46 +0700

    add

---
+++ CreatePostToPages.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+#-----------------------------------------------------------------------------
+# Name:        
+#
+# Purpose:
+#
+# Version:     1.1
+#
+# Author:
+#
+# Created:     06/02/2020
+# Updated:     06/02/2020
+#
+# Copyright:   -
+#
+#-----------------------------------------------------------------------------
+"""
+
+#Import lib
+
+class ImportPostTool:
+    def __init__(self, folder_product_files):
+        pass
+
+    def read_product_excel():
+        pass
+
+    def read_page_excel():
+        pass
+
+    def create_page_post_api();
+        pass
+
+    def upload_multi_photo_api():
+        pass
+
+    def import():
+        pass
+
+#Start application
+folder_product_files = "C:\\Users"
+folder_page_files = "C:\\Users"
+tool = ImportPostTool()
+tool.import()
+exit()
