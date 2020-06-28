1fd46cfdfcb1717f83264332fadd00c61bf7cf6e
Minh-Home <hungquocminh1989@gmail.com>
2020-06-28 18:53:51 +0700

    update

--- sftp.py
+++ sftp.py
@@ -1,13 +1,28 @@
 import pysftp
+from base64 import decodebytes
+import paramiko
 
 myHostname = "149.28.147.3"
 myPort = 15
 myUsername = "root"
-myPassword = ""
-myCnopts = pysftp.CnOpts()
-#myCnopts.hostkeys = None
+myPassword = "pA3?%tjGHy7ttUnj"
+myCnopts = pysftp.CnOpts(knownhosts='known_hosts')
+myCnopts.hostkeys = None
 
-with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword, private_key="c:\\private_key.pem", private_key_pass="123456", port=myPort, cnopts=myCnopts) as sftp:
+#keydata = b"""AAAAB3NzaC1yc2EAAAADAQABAAABAQC7Ol+V2SMygAknXSx4/fPKmre0rwBXH3j44BGVlZeOVOzstY0LGO9NhjX5eB1y4H1fJZKyc2PyKEe6KC77NmfOewlfLSIJo6KzJ95JC6KE+8rWbJrbleU3Bv+3+Vh4hcmh3R3e3oQ61ufpjfaLI2cp9MHXstkI1KPWvvFzFXmkG30QQdwsrk3Tob2GsNyzycXcMg7IBJInpML25y3LhHBd8fP37Yy12x9hZg+tAwF+IIUH7Ka0ygCB+mAShBa0h1Ju7B//G18gkYBehUGZ1Th2HPBU+wDYXoQew2gTkgLxwklDAo1MJujSI1atn6NepalYVDshzTf+oHm7IiirspBz"""
+#key = paramiko.RSAKey(data=decodebytes(keydata))
+#myCnopts = pysftp.CnOpts()
+#myCnopts.hostkeys.add('149.28.147.3', 'ssh-rsa', key)
+
+
+with pysftp.Connection(
+        host=myHostname,
+        username=myUsername,
+        password=myPassword,
+        #private_key="id_rsa",
+        port=myPort,
+        cnopts=myCnopts) as sftp:
+
 	print("Connection succesfully stablished ... ")
 
 	# Switch to a remote directory
