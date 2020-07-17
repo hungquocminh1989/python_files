17b6384822f7ece8337603b0c99c17bf41d5549a
Minh-Home <hungquocminh1989@gmail.com>
2020-07-18 00:09:46 +0700

    update

--- autotest.py
+++ autotest.py
@@ -40,23 +40,40 @@ import common
 class autotest:
     def __init__(self):
         self.shared = common.Shared()
-        self.vultr = common.VultrInstance('QTHEFETRVWWU7MTCVLSBHZL5E2ME6R4FX3NQ')
+        self.vultr = common.VultrInstance('')
         #self.remoting = common.Remoting("149.28.147.3", 15, "root", "")
         
     def run(self):
         #r = self.vultr.create_server()
         info = self.vultr.get_server_info('27676263')
         #print(info)
-        proxy = common.ProxyX(hostname=info['main_ip'], password=info["default_password"])
+        proxy = common.ProxyX(hostname=info['main_ip'], password=info["default_password"], port='15')
         proxy.start()
         #self.vultr.destroy_server('38784580')
         #self.vultr.get_startupscript_list()
-        #self.browser = common.SeleniumInstance()
-        #self.browser.set_redirect('https://api.ipify.org/')
+        self.browser = common.SeleniumInstance()
+        self.browser.set_redirect('https://api.ipify.org/')
+        self.browser.set_redirect('https://m.facebook.com/reg')
         #self.browser._save_cookies()
-        #self.browser.set_input_text('/html/body/main/form/dl[1]/dd/input', 'lixiladmin')
-        #self.browser.set_input_text('/html/body/main/form/dl[2]/dd/input', 'MSBuLBcQdGKL')
-        #self.browser.set_click('/html/body/main/form/input[2]')
+        self.browser.set_input_text('//*[@id="firstname_input"]', 'Nguyen')#Firstname
+        el = self.browser.set_input_text('//*[@id="lastname_input"]', 'Dung')#Lastname
+        self.browser.enter(el)#Next
+        
+        self.browser.set_input_combobox('//*[@id="month"]', value='3')#month
+        self.browser.set_input_combobox('//*[@id="day"]', value='15')#day
+        self.browser.set_input_combobox('//*[@id="year"]', value='1997')#year
+        self.browser.set_input_click('//*[@id="mobile-reg-form"]/div[8]/div[2]/button[1]')#Next
+        
+        self.browser.set_input_click('//*[@id="mobile-reg-form"]/div[10]/div/a[1]')#Link to mail
+
+        self.browser.set_input_text('//*[@id="contactpoint_step_input"]', 'oukgjg@gmail.com')#Mail
+        self.browser.set_input_click('//*[@id="mobile-reg-form"]/div[8]/div[2]/button[1]')#Next
+
+        self.browser.set_click('//*[@id="Female"]')#Female
+        self.browser.set_input_click('//*[@id="mobile-reg-form"]/div[8]/div[2]/button[1]')#Next
+
+        self.browser.set_input_text('//*[@id="password_step_input"]"]', 'aocufkeh123')#Password
+        self.browser.set_input_click('//*[@id="mobile-reg-form"]/div[8]/div[2]/button[1]')#Next
         #time.sleep(5000)
         #self.browser.set_input_click('/html/body/div/div/main/div[48]/form/div/div[1]/select/option[3222222]')
         
